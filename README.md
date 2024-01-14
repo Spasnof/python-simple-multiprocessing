f3d92bbe477a97faf18eb34978bea2977295428c
Joshua Proffitt <spasnof@gmail.com>
2024-01-13 22:35:31 -0800

    [refactor] simplify and explain

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # python-simple-multiprocessing
+For when you really just want something simple done in parallel. 
+
+Useful, appropriate for scripting simple low code ad-hoc tasks. Would not recommend for anything where you need more robust logging, error handling, retries, ect.
+
+
+# Credit
+Some of the code comes from this example here, modified for my purposes.
+https://stackoverflow.com/a/55995358/7239409
--- main.py
+++ main.py
@@ -1,68 +1,57 @@
 from multiprocessing import freeze_support
 from concurrent.futures import ProcessPoolExecutor
 import asyncio 
-import time
-import random
+from random import randrange
 import subprocess
-
-RANDOM_SLEEP = [
+ 
+# Concurrent executors to perform this work.
+MAX_WORKERS = 2
+# Some inputs that we can iterate over.
+RANDOM_SLEEP_UBOUNDS = [
     11,
     13,
     12,
     23,
-    13,
-    15
 ]
 
-def sleep_random_v2(sleep_max):
-    rn = random.randrange(0,sleep_max)
-    print(f'sleeping {rn}')
-    sp = subprocess.run(f'python sleep_random.py {rn}',shell=True)
-    print(f'done sleeping {rn}, exit code is {sp.returncode}')
-
 def sleep_random(sleep_max):
-    rn = random.randrange(0,sleep_max)
-    print(f'sleeping {rn}')
-    time.sleep(rn)
-    print(f'done sleeping {rn}')
+    """
+    This function can be whatever you want it to be, 
+    in this case we are just calling a subcommand to sleep for a few seconds.
+    Because sleep is a blocking function it makes for a good example.
+    """
+    random_number = randrange(0,sleep_max)
+    print(f'sleeping {random_number}')
+    # NOTE that shell=True is not secure, so don't use this for untrusted inputs.
+    # See https://docs.python.org/3/library/subprocess.html#security-considerations
+    sp = subprocess.run(f'python sleep_random.py {random_number}',shell=True)
+    print(f'Done sleeping {random_number}, exit code is {sp.returncode}')
 
-def blocking(delay):
-    time.sleep(delay+1)
-    return('EXECUTOR: Completed blocking task number ' + str(delay+1))
 
 def main():
 
-    async def mygen(u: int = 2):
-        i = 0
-        while i < u:
-            yield i
-            i += 1
-
-
-
     async def run_blocking(executor, task_no, delay):
-        print('MASTER: Sending to executor blocking task number '
-            + str(task_no))
-        result = await loop.run_in_executor(executor, sleep_random_v2, delay)
-        print(result)
+        print(f'MASTER: Sending to executor blocking task number {task_no}')
+        result = await loop.run_in_executor(executor, sleep_random, delay)
         print('MASTER: Well done executor - you seem to have completed '
             'blocking task number ' + str(task_no))
 
-    async def non_blocking(loop):
+    async def non_blocking():
         tasks = []
+        # here we use simple incrementing id's but nothing stopping you from using job uuid's ect.
         task_no = 1
-        with ProcessPoolExecutor(max_workers=5) as executor:
-            for i in RANDOM_SLEEP:
-                # print(i)
-                # spawn the task and let it run in the background
+        with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            for i in RANDOM_SLEEP_UBOUNDS:
+                # append the task to an empty array of tasks.
                 tasks.append(asyncio.create_task(
                     run_blocking(executor=executor, task_no=task_no, delay=i)))
                 task_no += 1
-            # if there was an exception, retrieve it now
+            # if there was an exception prior to task creation we would get it here
             await asyncio.gather(*tasks)
 
+    # instantiate the loop and just run everything inside of that
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(non_blocking(loop))
+    loop.run_until_complete(non_blocking())
 
 
 if __name__ == '__main__':
